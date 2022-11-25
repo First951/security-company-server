def15f8a34402e064716f36501681402005f18ca
VacuouS <vacuous21@gmail.com>
2022-11-25 13:35:03 +0300

    chore: Добавить классы сущностей БД

---
+++ src/main/java/ru/first/entity/Organization.java
@@ -0,0 +1,10 @@
+package ru.first.entity;
+
+/**
+ * Сущность Организация из БД
+ */
+public class Organization {
+    private int id;
+    private int name;
+    private int adress;
+}
---
+++ src/main/java/ru/first/entity/TypeOfProps.java
@@ -0,0 +1,4 @@
+package ru.first.entity;
+
+public class TypeOfProps {
+}
