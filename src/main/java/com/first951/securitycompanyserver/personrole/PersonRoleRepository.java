9c599fb9875faf378621daabf8571cd715e7d5b5
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-08 22:04:12 +0300

    feat: Сущность и Репозиторий таблицы Роль-Человек

---
+++ src/main/java/com/first951/securitycompanyserver/personrole/PersonRoleRepository.java
@@ -0,0 +1,7 @@
+package com.first951.securitycompanyserver.personrole;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRoleRepository extends CrudRepository<PersonRoleEntity, Integer> {
+
+}
