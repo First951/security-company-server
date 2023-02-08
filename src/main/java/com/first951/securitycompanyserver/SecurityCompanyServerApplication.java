557b6d1a4b7ca3f5eff51e51d57efc6c70391272
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-08 15:23:03 +0300

    build: Пустой проект с start.spring.io

---
+++ src/main/java/com/first951/securitycompanyserver/SecurityCompanyServerApplication.java
@@ -0,0 +1,13 @@
+package com.first951.securitycompanyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCompanyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCompanyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first951/securitycompanyserver/SecurityCompanyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first951.securitycompanyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCompanyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
