7e7534ee1c97f3d9f20f9a28a009eed8c350b586
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-09 13:07:38 +0300

    feat: Сущность и Репозиторий для DutyRegister

---
+++ src/main/java/com/first951/securitycompanyserver/dutyregister/DutyRegisterEntity.java
@@ -0,0 +1,34 @@
+package com.first951.securitycompanyserver.dutyregister;
+
+import com.first951.securitycompanyserver.person.PersonEntity;
+import com.first951.securitycompanyserver.post.PostEntity;
+import jakarta.persistence.*;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "duty_register")
+public class DutyRegisterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private PostEntity post;
+
+    @ManyToOne
+    @JoinColumn(name = "guard_id", nullable = false)
+    private PersonEntity guard;
+
+    @Column(name = "begin", nullable = false)
+    private Timestamp begin;
+
+    @Column(name = "end", nullable = false)
+    private Timestamp end;
+
+}
