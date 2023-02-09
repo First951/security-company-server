7b28dfed997e008ceed820689e39d795e05b7447
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-09 12:47:10 +0300

    feat: Сущность и репозиторий для Object

---
+++ src/main/java/com/first951/securitycompanyserver/object/ObjectRepository.java
@@ -0,0 +1,7 @@
+package com.first951.securitycompanyserver.object;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ObjectRepository extends CrudRepository<ObjectEntity, Integer> {
+
+}
