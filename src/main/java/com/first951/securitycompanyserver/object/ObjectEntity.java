78fa0bf202291bd99d7a07d32a4105f6e343548b
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-10 12:23:29 +0300

    feat: Реализация Service для Object

--- src/main/java/com/first951/securitycompanyserver/object/ObjectEntity.java
+++ src/main/java/com/first951/securitycompanyserver/object/ObjectEntity.java
@@ -2,9 +2,13 @@ package com.first951.securitycompanyserver.object;
 
 import com.first951.securitycompanyserver.organization.OrganizationEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "object")
+@Getter
+@Setter
 public class ObjectEntity {
 
     @Id
