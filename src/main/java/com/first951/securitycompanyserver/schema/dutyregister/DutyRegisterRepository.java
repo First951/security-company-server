3983020562c498acf83ae0095b87f77d4e05aaf7
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-03-09 17:14:54 +0300

    ref: Удалине ненужных сущностей регистров

--- src/main/java/com/first951/securitycompanyserver/schema/dutyregister/DutyRegisterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.first951.securitycompanyserver.schema.dutyregister;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DutyRegisterRepository extends CrudRepository<DutyRegisterEntity, Integer> {
-
-}
